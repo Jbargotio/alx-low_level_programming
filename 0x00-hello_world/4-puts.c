f166642fd67df0d4297e5c85867c28f895514457
Jbargotio <jbargotio@gmail.com>
2022-03-10 08:37:13 -0800

    Adding 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
- * A program that prints "Programming is like building a multilingual puzzle
+/**
  * main - Entry point
  *
- * Return: Always 0 (Success)*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
