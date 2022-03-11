b86d00858595a0549efab5aacd7f936739bdd16c
Jbargotio <jbargotio@gmail.com>
2022-03-11 04:24:05 -0800

    Adding 6-print_bumberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * 0-9 putchar
+ *
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar((a % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
