47f83eadf92021b1ea0d07862afada5dce5c9d00
Jbargotio <jbargotio@gmail.com>
2022-03-11 03:13:21 -0800

    Adding 2-alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-char x;
-    for(x = 'a'; x <= 'z'; x++)
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
 {
-    putchar(x);
+	putchar(x);
 }
 putchar('\n');
 return (0);
