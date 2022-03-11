ae4253c89ce03393966f0bdcd2c30b0c05e50f00
Jbargotio <jbargotio@gmail.com>
2022-03-11 08:52:21 -0800

    Adding 100-print_combs3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	int first, last;
-	for ( first = 0; first < 9; first++)
+
+	for (first = 0; first < 9; first++)
 	{
 		for (last = first + 1; last < 10; last++)
 		{
-			putchar(( first % 10) + '0');
+			putchar((first % 10) + '0');
 			putchar((last % 10) + '0');
 
 			if (first == 8 && last == 9)
