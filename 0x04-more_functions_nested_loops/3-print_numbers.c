996c81105b5919a9d4faffd60fc2749856b2f521
Jbargotio <jbargotio@gmail.com>
2022-03-17 01:22:01 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints nmubers from 0-9
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <=57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
