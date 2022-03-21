edf896557c284b919467f78e34334ce55b794bdc
Jbargotio <jbargotio@gmail.com>
2022-03-21 05:20:34 -0700

    Adding 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer
+ * to an int
+ * and resets to 98
+ * @n:pointer to 98.
+ *
+ * Return: 1 if the number is a digit, 0 otherwise.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
